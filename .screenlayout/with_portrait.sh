89ab0d84bb8a154279843d99f11938fdcb60c356
Fabrizio Sestito <fabrizio.sestito@gmail.com>
2021-05-22 09:06:43 +0200

    Add autorandr generated layout

---
+++ .screenlayout/with_portrait.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --primary --mode 2560x1440 --pos 1440x847 --rotate normal --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate left --output eDP-1-1 --off
