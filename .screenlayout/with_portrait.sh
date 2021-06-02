777948d43a491dffc4adcab1e03f282c0335300c
Fabrizio Sestito <fabrizio.sestito@suse.com>
2021-06-02 19:27:14 +0200

    New font!

--- .screenlayout/with_portrait.sh
+++ .screenlayout/with_portrait.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --primary --mode 2560x1440 --pos 1440x847 --rotate normal --output HDMI-0 --mode 2560x1440 --pos 0x0 --rotate left --output eDP-1-1 --off
+xrandr --output DP-0 --off --output DP-1.3 --primary --mode 2560x1440 --pos 1440x847 --rotate normal --output DP-1.1 --mode 2560x1440 --pos 0x0 --rotate left --output eDP-1-1 --off
