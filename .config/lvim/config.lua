71fc6a5233d4118cd3d1e8bf0dc69f78aadb95df
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-10-25 06:13:37 +0200

    lvim

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+lvim.colorscheme = "rose-pine"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
@@ -160,7 +160,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-  { "pradyungn/Mountain", rtp = "vim" },
+  { "mcchrish/zenbones.nvim" },
+  { "rose-pine/neovim" }
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
